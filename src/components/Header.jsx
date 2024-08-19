1c919e61a503eaec48fbbfcc97d0c249af194bce
Sebastian <sebasrojasm1@gmail.com>
2024-08-19 15:15:31 -0500

    The project was inicializated with React, and was created the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import '../assets/styles/Header.css';
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="navBarLogo">
+          <a href="#">Portfolio</a>
+        </div>
+        <input type="checkbox" id="check" className="checkbox" />
+        <div className="opcionesNavbar">
+          <li><a href="#principal">Sobre mi</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#title-project">Proyectos</a></li>
+          <li><a href="#footer">Contacto</a></li>
+
+          <label htmlFor="check" className="closeMenu">
+            <i className="fa-solid fa-xmark" style={{ color: "#AD974F" }}></i>
+          </label>
+        </div>
+
+        <label htmlFor="check" className="openMenu">
+          <i className="fa-solid fa-bars" style={{ color: "#AD974F" }}></i>
+        </label>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
